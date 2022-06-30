153e27339fcae416b0cf08c3b34be375158ed041
Tess Grankina <taisa.grankina@gmail.com>
2022-06-30 14:48:49 +0200

    Fix the title: check hot-fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Wine Cellar App<a href="https://nextjs.org">Next.js!</a>
+        <h1 className={styles.title}>Wine Cellar App
         </h1>
 
         <p className={styles.description}>
